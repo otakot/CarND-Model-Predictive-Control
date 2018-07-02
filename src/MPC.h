db05278339f7e138592c72f0583acffacdfe76d1
oleksandr.kurtseba <oleksandr.kurtseba@192.168.0.49>
2018-07-02 16:16:21 +0200

    Implementation of MPC controller + Eigen lib

---
+++ src/MPC.h
@@ -0,0 +1,81 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+constexpr std::size_t N = 20; // number of steps
+constexpr double dt = 0.05; // duration of one step, in seconds
+
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+constexpr double Lf = 2.67;
+
+constexpr double ref_v = 35; // vehicle velocity, mph
+
+// The solver takes all the state variables and actuator
+// variables in a singular vector. Thus, we should to establish
+// when one variable starts and another ends to make our lifes easier.
+constexpr size_t x_start = 0;
+constexpr size_t y_start = x_start + N;
+constexpr size_t psi_start = y_start + N;
+constexpr size_t v_start = psi_start + N;
+constexpr size_t cte_start = v_start + N;
+constexpr size_t epsi_start = cte_start + N;
+constexpr size_t delta_start = epsi_start + N;
+constexpr size_t a_start = delta_start + N - 1;
+
+constexpr unsigned STEERING_TRANSITION_SMOOTHNESS = 500;
+constexpr unsigned ACCELERATION_SMOOTHNESS = 20;
+
+
+
+struct MpcOutput {
+  public:
+
+    vector<double> X;
+    vector<double> Y;
+    vector<double> DELTA; // orientation change
+    vector<double> A; // acceleration
+
+    /*
+     * Return the predicted Steering Angle for given delay, in milliseconds
+     */
+    double GetSteeringAngle(unsigned delayed_state_index) const{
+      // orientation angle scale in simulator is inverted to calculated by controller
+      return -DELTA.at(delayed_state_index);
+    }
+
+    double GetThrottle(unsigned delayed_state_index) const{
+      return A.at(delayed_state_index);
+    }
+};
+
+class MPC {
+ public:
+  MPC(unsigned actuators_latency);
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  MpcOutput Solve(const Eigen::VectorXd& state, const Eigen::VectorXd& coeffs);
+
+  unsigned GetDelayedStateIndex() const;
+
+  private:
+    unsigned delated_state_index_;
+};
+
+#endif /* MPC_H */
---
+++ src/main.cpp
@@ -0,0 +1,242 @@
+#include <math.h>
+#include <uWS/uWS.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <utility>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+#include "MPC.h"
+#include "json.hpp"
+
+// for convenience
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+constexpr double MAX_TURN_ANGLE = 25.0; // in degrees
+constexpr unsigned ACTUATORS_LATENCY = 100; // in milliseconds
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.rfind("}]");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
+// Evaluate a polynomial.
+double polyeval(Eigen::VectorXd coeffs, double x) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); i++) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+// Fit a polynomial.
+// Adapted from
+// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                        int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); i++) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); j++) {
+    for (int i = 0; i < order; i++) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+  return result;
+}
+
+
+const std::pair<Eigen::VectorXd, Eigen::VectorXd> TransformTrajectoryMapToVehicle(
+  double x, double y, double psi, const vector<double>& ptsx, const vector<double>& ptsy) {
+
+  // make sure that number of passed x and y values is equal
+  assert(ptsx.size() == ptsy.size());
+
+  std::size_t points_num = ptsx.size();
+
+  Eigen::VectorXd points_x(points_num);
+  Eigen::VectorXd points_y(points_num);
+
+  for (std::size_t i=0; i < points_num; ++i){
+    points_x(i) =  cos(psi) * (ptsx[i] - x) + sin(psi) * (ptsy[i] - y);
+    points_y(i) =  -sin(psi) * (ptsx[i] - x) + cos(psi) * (ptsy[i] - y);
+  }
+
+  return std::make_pair(points_x, points_y);
+
+}
+
+
+double CalculateCrossTrackError(const Eigen::VectorXd& coeffs) {
+  // in car coordinate system x position of the car is always 0.0
+  return polyeval(coeffs, 0.0);
+}
+
+
+double CalculateOrientationError(const Eigen::VectorXd& coeffs) {
+  // orientation error epsi is calculated using following equations:
+  // epsi = psi - arctan(f'(x)). Since in car coordinate system current psi is always 0, we get
+  // epsi = -atan(f'(x))
+  // f(x) = coeffs[0] + coeffs[1]*x, thus f'(x) = coeffs[1]
+  // This results in equation: epsi = -atan(coeffs[1])
+  return -atan(coeffs[1]);
+}
+
+Eigen::VectorXd GetCurrentState(const double velocity, const double cte, const double epsi) {
+ Eigen::VectorXd state(6);
+ // in car coordinate system current position and orientation are always 0
+ state << 0, 0, 0, velocity, cte, epsi;
+ return state;
+}
+
+
+int main() {
+  uWS::Hub h;
+
+  // MPC is initialized here!
+  MPC mpc(ACTUATORS_LATENCY);
+
+  // index of state vector in predicted MPC solution that reflects the matches of actuators
+  const unsigned delayed_state_index = mpc.GetDelayedStateIndex();
+
+  h.onMessage([&mpc, &delayed_state_index](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                     uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    string sdata = string(data).substr(0, length);
+    cout << sdata << endl;
+    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+      string s = hasData(sdata);
+      if (s != "") {
+        auto j = json::parse(s);
+        string event = j[0].get<string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          vector<double> ptsx = j[1]["ptsx"];
+          vector<double> ptsy = j[1]["ptsy"];
+          double px = j[1]["x"];
+          double py = j[1]["y"];
+          double psi = j[1]["psi"];
+          double v = j[1]["speed"];
+
+
+          // transform trajectory points from map coordinate system to vehicle coordinate system
+          const std::pair<Eigen::VectorXd, Eigen::VectorXd> reference_trajectory = TransformTrajectoryMapToVehicle(px, py, psi, ptsx, ptsy);
+          const Eigen::VectorXd& reference_points_x = reference_trajectory.first;
+          const Eigen::VectorXd& reference_points_y = reference_trajectory.second;
+
+          // fit a 3rd order polynomial from trajectory points
+          const Eigen::VectorXd coeffs = polyfit(reference_points_x, reference_points_y, 3);
+
+          // calculate cte
+          const double cte = CalculateCrossTrackError(coeffs);
+
+          // calculate ephi
+          const double epsi = CalculateOrientationError(coeffs);
+
+          const Eigen::VectorXd current_state = GetCurrentState(v, cte, epsi);
+
+          // compute optimal trajectory for vehicle
+          const MpcOutput mpc_output = mpc.Solve(current_state, coeffs);
+
+
+          json msgJson;
+
+          // steering angle with respect to actuators latency, in [-1, 1] scale
+          const double steer_value = mpc_output.GetSteeringAngle(delayed_state_index);
+          // normalize calculated steer value (in radians) to [-1, 1] scale before sending back to simulator.
+          msgJson["steering_angle"] = steer_value/(deg2rad(MAX_TURN_ANGLE));
+
+          // level of throttle with respect to actuators latency, in [-1, 1] scale
+          msgJson["throttle"] = mpc_output.GetThrottle(delayed_state_index);
+
+          // predicted trajectory points(in vehicle's coordinate system), will be displayes as Green line in simulator
+          msgJson["mpc_x"] = mpc_output.X;
+          msgJson["mpc_y"] = mpc_output.Y;
+
+          // reference trajectory points (in vehicle's coordinate system). will be displayed as Yellow line
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          for (unsigned i=0 ; i < reference_points_x.size(); ++i) {
+            next_x_vals.push_back(reference_points_x(i));
+            next_y_vals.push_back(reference_points_y(i));
+          }
+
+          msgJson["next_x"] = next_x_vals;
+          msgJson["next_y"] = next_y_vals;
+
+
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+          std::cout << msg << std::endl;
+          // Latency.  The purpose is to mimic real driving conditions where
+          // the car does actuate the commands instantly.
+          this_thread::sleep_for(chrono::milliseconds(ACTUATORS_LATENCY));
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+  });
+
+  // We don't need this since we're not using HTTP but if it's removed the
+  // program
+  // doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+                     size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1) {
+      res->end(s.data(), s.length());
+    } else {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                         char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port)) {
+    std::cout << "Listening to port " << port << std::endl;
+  } else {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+}
